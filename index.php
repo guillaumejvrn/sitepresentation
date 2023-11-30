3408b8c35a71e5f986b3e386c1f261f07c690000
guillaumejvrn <guillaume.jenvrin@sts-sio-caen.info>
2023-11-30 10:35:44 +0100

    mathis

--- index.php
+++ index.php
@@ -34,10 +34,23 @@
             <div id="compet">
                 <div id="item">
                     <?php
-                    require_once("yaml.yaml");
-                    $data=my_yaml_parse_file('langagedeprog');
-                    foreach($data["langagedeprog"]AS $prog){
-                        echo "<p>".ucfirst($prog["java"])." : ".$prog["cpp"]." ".$prog["html"]." ".$prog["css"]." ".$prog["javascript"]." ".$prog["php"]." ".$prog["mysql"]."</p>\n";
+                    require_once("yaml/yaml.php");
+                    $data=yaml_parse_file('yaml.yaml');
+                    print_array($data);
+                    foreach($data AS $domaine){
+                        echo "<h1>".$domaine['domaine']."</h1>";
+                        foreach($domaine["competences"] AS $competence=> $niveau){
+                            echo "<p>".$competence;
+                            $star = "solid";
+                            for($i=1; $i < 6 ;$i++){
+                                echo '<img src="images/star-'.$star.'.svg" class="star">';
+                                if($i >= $niveau){
+                                    $star ="regular";
+                                }
+                            }
+                            echo "</p>";
+                        }
+                        //echo "<p>".ucfirst($prog["java"])." : ".$prog["cpp"]." ".$prog["html"]." ".$prog["css"]." ".$prog["javascript"]." ".$prog["php"]." ".$prog["mysql"]."</p>\n";
                     }
                     ?>
                 </div>
